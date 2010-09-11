b9ce78c86e3c636b96708394b368c8ad9108a4d4
Stuart Sierra <mail@stuartsierra.com>
2010-09-11 09:40:40 -0400

    Remove extraneous ELPA setup from customizations.el

--- customizations.el
+++ customizations.el
@@ -29,30 +29,9 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(default ((t (:height 140 :foundry "Anonymous_Pro"))))
  '(linum ((t (:inherit (shadow default) :foreground "#444" :height 110)))))
 
 (put 'narrow-to-region 'disabled nil)
 
 (put 'downcase-region 'disabled nil)
-
-
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
-
-
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
