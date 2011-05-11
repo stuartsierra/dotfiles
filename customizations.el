c7bce7ba69a98cf3f33cb0b7689ee65b073f4e41
Stuart Sierra <mail@stuartsierra.com>
2011-05-11 14:09:07 -0400

    Update customizations.el

--- customizations.el
+++ customizations.el
@@ -9,6 +9,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-autoface-mode nil)
  '(aquamacs-customization-version-id 208 t)
  '(aquamacs-tool-bar-user-customization (quote ((16777249 new-file open-file recent-files save-buffer aquamacs-print nil undo redo cut copy paste isearch-forward nil customize help))) t)
  '(backward-delete-char-untabify-method (quote all))
@@ -19,6 +20,8 @@
  '(linum-format "%3d ")
  '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode (quote regular) t)
+ '(org-special-ctrl-a/e t)
+ '(org-startup-folded (quote showeverything))
  '(org-todo-keywords (quote ((sequence "TODO" "INPROGRESS" "DONE"))))
  '(safe-local-variable-values (quote ((eval define-clojure-indent (to-data (quote defun))))))
  '(slime-kill-without-query-p t)
@@ -33,8 +36,10 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :foundry "Anonymous_Pro"))))
+ '(default ((t (:stipple nil :background "#141414" :foreground "#CACACA" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Anonymous Pro"))))
  '(linum ((t (:inherit (shadow default) :background "black" :foreground "#444"))))
+ '(magit-diff-add ((((class color) (background dark)) (:foreground "green"))))
+ '(mode-line-inactive ((t (:inherit aquamacs-variable-width :background "grey10" :foreground "grey40" :box (:line-width -1 :color "grey20") :strike-through nil :underline nil :slant normal :weight normal :width normal))))
  '(org-hide ((((background dark)) (:foreground "#141414"))))
  '(outline-4 ((t (:foreground "#8F8A80")))))
 
