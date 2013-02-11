da10adccebf898d676724de77cf768954f0b3412
Stuart Sierra <mail@stuartsierra.com>
2013-02-11 08:44:42 -0500

    emacs custom: change default font size

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -58,4 +58,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:height 200 :foundry "apple" :family "Anonymous_Pro")))))
+ '(default ((t (:height 180 :foundry "apple" :family "Anonymous_Pro")))))
