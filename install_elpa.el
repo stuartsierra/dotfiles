31939ee00165c4a285c8a49ff8455890c319af3d
Stuart Sierra <mail@stuartsierra.com>
2013-06-12 09:53:01 -0400

    Update install_elpa.el script for Emacs 24 / Marmalade

--- install_elpa.el
+++ install_elpa.el
@@ -1,11 +1,15 @@
 (setq user-init-file "/tmp/.emacs")
 
+;; Marmalade: http://marmalade-repo.org/
+(require 'package)
+(add-to-list 'package-archives
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
 (package-refresh-contents)
 
-(package-install 'css-mode)
 (package-install 'gist)
-(package-install 'javascript)
 (package-install 'paredit)
 (package-install 'ruby-mode)
-(package-install 'ruby-electric)
 (package-install 'smex)
