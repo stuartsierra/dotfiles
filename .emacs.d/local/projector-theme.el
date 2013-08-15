9b76263f7bab79d72c1b283a542abe34f7621ee6
Stuart Sierra <mail@stuartsierra.com>
2013-08-14 23:39:46 -0400

    Add projector theme for presentations

---
+++ .emacs.d/local/projector-theme.el
@@ -0,0 +1,24 @@
+(deftheme projector
+  "Created 2013-08-14.")
+
+(custom-theme-set-faces
+ 'projector
+ '(highlight ((t (:background "darkseagreen2"))))
+ '(font-lock-builtin-face ((t (:foreground "dark slate blue"))))
+ '(font-lock-comment-delimiter-face ((default (:inherit (font-lock-comment-face)))))
+ '(font-lock-comment-face ((t (:foreground "Firebrick"))))
+ '(font-lock-constant-face ((t (:foreground "purple"))))
+ '(font-lock-doc-face ((t (:foreground "DarkGreen"))))
+ '(font-lock-function-name-face ((t (:foreground "Blue"))))
+ '(font-lock-keyword-face ((t (:foreground "dark slate blue"))))
+ '(font-lock-negation-char-face ((t nil)))
+ '(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face)))))
+ '(font-lock-regexp-grouping-backslash ((t (:inherit (bold)))))
+ '(font-lock-regexp-grouping-construct ((t (:inherit (bold)))))
+ '(font-lock-string-face ((t (:foreground "DarkGreen"))))
+ '(font-lock-type-face ((t nil)))
+ '(font-lock-variable-name-face ((t nil)))
+ '(font-lock-warning-face ((t (:inherit (error)))))
+ '(default ((t (:background "white" :foreground "black")))))
+
+(provide-theme 'projector)
