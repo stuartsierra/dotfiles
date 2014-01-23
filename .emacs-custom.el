57ac681710b371ebd441d86a8739bd8d64970759
Stuart Sierra <mail@stuartsierra.com>
2014-01-23 14:49:28 -0500

    Emacs custom: commit additional 'safe' themes

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -15,7 +15,7 @@
  '(backup-directory-alist (quote (("." . "/tmp/emacs-backups"))))
  '(backward-delete-char-untabify-method (quote all))
  '(column-number-mode t)
- '(custom-safe-themes (quote ("1875632bec1cc9954c7bfe7babb5177a9f2ecda73d255041a24c7e6428059ed5" "26a2398fa2c7bdaf5e456484ff45fade498e9e500ebad2ddf84d3509a952e98d" default)))
+ '(custom-safe-themes (quote ("2b91408c2bc8b2e4857f93bd1f8e00cc6104e5ea7ee77738dcfc5f0d4bc4709f" "1875632bec1cc9954c7bfe7babb5177a9f2ecda73d255041a24c7e6428059ed5" "26a2398fa2c7bdaf5e456484ff45fade498e9e500ebad2ddf84d3509a952e98d" default)))
  '(delete-old-versions t)
  '(dired-listing-switches "-alg")
  '(fringe-mode nil nil (fringe))
