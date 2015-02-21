99ce4696b0e4469e3b00e685034ecda1511a9e90
Stuart Sierra <mail@stuartsierra.com>
2015-02-21 13:17:49 -0500

    Emacs: remove hackish org-to-confluence
    Instead use library ox-confluence

--- .emacs.d/local/init.el
+++ .emacs.d/local/init.el
@@ -223,15 +223,6 @@
 (global-set-key "\C-con" 'todays-daypage)
 (global-set-key "\C-coN" 'find-daypage)
 
-(defun org-to-confluence ()
-  "Convert region from org-mode to Confluence wiki syntax"
-  (interactive)
-  (save-excursion
-   (let ((start (region-beginning))
-         (end (region-end)))
-     (replace-regexp "^\\*\\*\\* \\(.+\\)$" "\nh2. \\1\n" nil start end)
-     (replace-regexp "^\\*\\*\\*" "" nil start end))))
-
 (defun my-agenda ()
   (interactive)
   (org-agenda nil "n"))
