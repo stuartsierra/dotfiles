9e0a58ffa303e3dbf3e76dc8ca62b0af023a2176
Stuart Sierra <mail@stuartsierra.com>
2015-09-18 17:27:02 -0400

    Emacs/CIDER: log NREPL messages to buffer

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -40,6 +40,7 @@
  '(magit-process-popup-time -1)
  '(magit-set-upstream-on-push t)
  '(menu-bar-mode nil)
+ '(nrepl-log-messages t)
  '(ns-pop-up-frames nil)
  '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode (quote regular) t)
