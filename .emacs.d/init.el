54d029c02754ef6ac0b931fd4f305e7e6a0512ce
Stuart Sierra <mail@stuartsierra.com>
2017-09-02 11:43:12 -0400

    Emacs init: package-initialize

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,9 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 ;; Load my local copy of org-mode, not the version included with Emacs
 (push (expand-file-name "vendor/org-mode/lisp" user-emacs-directory) load-path)
 
