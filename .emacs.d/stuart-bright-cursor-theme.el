6b6af58f793f61d4e4b7c317f611e4e3253489cd
Stuart Sierra <mail@stuartsierra.com>
2019-12-07 17:15:19 -0500

    Add my bright-cursor theme extension

---
+++ .emacs.d/stuart-bright-cursor-theme.el
@@ -0,0 +1,8 @@
+(deftheme stuart-bright-cursor
+  "Created 2019-12-07.")
+
+(custom-theme-set-faces
+ 'stuart-bright-cursor
+ '(cursor ((t (:background "#ffff00")))))
+
+(provide-theme 'stuart-bright-cursor)
