9609ba75fcaacb9821ffa4ff724ad1d856c7824f
Stuart Sierra <mail@stuartsierra.com>
2019-12-14 16:10:55 -0500

    Emacs: Separate file just to install packages

---
+++ .emacs.d/install-my-packages.el
@@ -0,0 +1,78 @@
+(package-initialize)
+
+(defun stuart/all-packages-installed-p (packages)
+  (cl-loop for pkg in packages
+	   when (not (package-installed-p pkg)) do (cl-return nil)
+	   finally (cl-return t)))
+
+(defun stuart/install-all-packages (packages)
+  (unless (stuart/all-packages-installed-p packages)
+    (package-refresh-contents)
+    (dolist (pkg packages)
+      (when (not (package-installed-p pkg))
+	(package-install pkg)))))
+
+(setq stuart/stable-packages
+  '(
+    ag
+    cider
+    clojure-mode
+    clojure-mode-extra-font-locking
+    color-theme-sanityinc-solarized
+    exec-path-from-shell
+    find-file-in-project
+    flycheck-clj-kondo
+    gh
+    gist
+    git-commit
+    gnuplot
+    go-eldoc
+    go-mode
+    graphviz-dot-mode
+    groovy-mode
+    htmlize
+    ido-vertical-mode
+    inf-clojure
+    ledger-mode
+    lua-mode
+    magit
+    markdown-mode
+    multiple-cursors
+    org-plus-contrib
+    paredit
+    rainbow-delimiters
+    ruby-mode
+    s
+    smex
+    solarized-theme
+    typescript-mode
+    typo
+    xml-rpc
+    ))
+
+(setq stuart/unstable-packages
+  '(graphql-mode
+    hlinum
+    linum-off
+    ob-restclient
+    restclient
+    ))
+
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("org" . "https://orgmode.org/elpa/") t)
+
+(stuart/install-all-packages stuart/stable-packages)
+
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(stuart/install-all-packages stuart/unstable-packages)
+
+;; (unless (stuart/all-packages-installed-p)
+;;   (message "%s" "Refreshing package database...")
+;;   (package-refresh-contents)
+;;   (dolist (pkg stuart/packages)
+;;     (when (not (package-installed-p pkg))
+;;       (package-install pkg))))
