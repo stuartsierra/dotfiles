ff745cc4172cef3133ecd52128429334e9054bc3
Stuart Sierra <mail@stuartsierra.com>
2022-03-18 11:23:06 -0400

    Emacs: put all backup files in a temp directory

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
+ '(backup-directory-alist '(("." . "/tmp/emacs-backups")))
  '(cider-repl-display-help-banner nil)
  '(exec-path-from-shell-arguments '("-l"))
  '(ffip-prefer-ido-mode t)
